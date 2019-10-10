fb12ded48b871c527a72d43fb0e92f7ca33b8130
KubaZawierucha <j.zawierucha97@gmail.com>
2019-10-10 12:46:51 +0200

    Initial commit - Main menu + Front body activity.

---
+++ app/src/main/java/com/kubazawierucha/powerfulbodyapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.kubazawierucha.powerfulbodyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button goToBodyActivityBtn = findViewById(R.id.goToBodyScreenBtn);
+        goToBodyActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bodyActivityIntent = new Intent(getApplicationContext(), BodyActivity.class);
+                startActivity(bodyActivityIntent);
+            }
+        });
+    }
+
+
+}
