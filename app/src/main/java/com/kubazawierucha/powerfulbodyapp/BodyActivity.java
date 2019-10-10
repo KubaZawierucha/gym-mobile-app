9ff5a1c9aae37dded611c9da7e3575e8a9b7ab75
KubaZawierucha <j.zawierucha97@gmail.com>
2019-10-10 21:45:16 +0200

    - Front body img added. - Reversed body img added. - Fragments transition added.

--- app/src/main/java/com/kubazawierucha/powerfulbodyapp/BodyActivity.java
+++ app/src/main/java/com/kubazawierucha/powerfulbodyapp/BodyActivity.java
@@ -1,7 +1,7 @@
 package com.kubazawierucha.powerfulbodyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class BodyActivity extends AppCompatActivity {
@@ -10,5 +10,10 @@ public class BodyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.body_fragment_container, new FrontBodyFragment());
+        fragmentTransaction.commit();
     }
+
 }
